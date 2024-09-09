56b5b82d87d36aac338190e66c6d62d31202f70f
Sami Munir <sami.munir2001@gmail>
2024-09-09 13:29:10 -0400

    using input() function to save user's name into variable & then print it out

---
+++ Harvard CS50 Intro Python Course/Variables/variables/src/main.py
@@ -0,0 +1,11 @@
+# Harvard CS50's Intro Python Programming Course
+# freeCodeCamp.org
+# September 9th, 2024
+# variables
+#   main.py
+
+# ask a user for their name.
+name = input("What's your name?\n--> ")
+
+# say hello to the user.
+print("\nHello", name)
