bae70bf443e055b69be98a59473be73ac44c26ef
Sami Munir <sami.munir2001@gmail>
2024-09-09 13:56:52 -0400

    working with Python str function strip() to remove any whitespace from user input name

--- Harvard CS50 Intro Python Course/Variables/variables/src/main.py
+++ Harvard CS50 Intro Python Course/Variables/variables/src/main.py
@@ -9,3 +9,8 @@ name = input("What's your name?\n--> ")
 
 # say hello to the user.
 print("\nHello,", name, sep = ' ', end = '\n')
+
+# say hello to user using format_string
+# - using split() to remove any whitespace on left/right
+name = name.strip()
+print(f"\nHello, {name}")
