21aa82014f57a4c9a02f8b2e98775be84600b0a9
Sami Munir <sami.munir2001@gmail>
2024-09-10 14:10:06 -0400

    working on main.py (implementing GET & POST routes & functions

--- Projects/Full Stack Contact List/full-stack-contact-list/backend/main.py
+++ Projects/Full Stack Contact List/full-stack-contact-list/backend/main.py
@@ -1,11 +1,39 @@
+# CRUD
+# - create
+#   - id
+#   - firstName
+#   - lastName
+#   - email
+# - read
+# - update
+#   - id
+# - delete
+#   - id
+
+# CRUD endpoints
+#   localhost:5000/create_contact
+#   localhost:5000/contacts
+#   localhost:5000/update_contact/{id}
+#   localhost:5000/delete_contact/{id}
+
+# Request
+# - type (GET, POST, PATCH, DELETE)
+# - JSON (data that comes alongside a request)
+#   - JSON is an object of key/value pairs
+
+# Response
+# - status (success, error/failure)
+
 from flask import request, jsonify
 from config import app, db
 from models import Contact
 
 @app.route("/contacts", methods = ["GET"])
 def get_contacts():
+    # getting all the different contacts in our database (Python objects).
     contacts = Contact.query.all()
-    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    # for each Python Contact object -> convert to JSON using to_json() function.
+    json_contacts = list(map(lambda contact: contact.to_json(), contacts))
     return jsonify({"contacts": json_contacts})
 
 @app.route("/create_contact", methods = ["POST"])
@@ -18,7 +46,10 @@ def create_contact():
         return jsonify({"message": "You must include a first name, last name, and email."}), 400
     new_contact = Contact(first_name = first_name, last_name = last_name, email = email)
     try:
+        # staging area (ready to write to database).
         db.session.add(new_contact)
+        # write whatever in staging area to the database.
+        # - encapsulate within try/catch block to handle any errors.
         db.session.commit()
     except Exception as e:
         return jsonify({"message": str(e)}), 400
@@ -51,6 +82,10 @@ def delete_contact(user_id):
     return jsonify({"message": "User deleted."}), 200
 
 if __name__ == "__main__":
+    # instantiating database
+    # - when we start the application, get the context.
+    # - then create all the different models defined in our database.
+    #   - spin up the database if not already existing.
     with app.app_context():
         db.create_all()
     app.run(debug = True)
