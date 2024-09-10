a378c7664ffd3039f187ed78837f597c93cb2b26
Sami Munir <sami.munir2001@gmail>
2024-09-10 13:48:43 -0400

    implementing models.py to using ORM (Python Contact object --> database model)

--- Projects/Full Stack Contact List/full-stack-contact-list/backend/models.py
+++ Projects/Full Stack Contact List/full-stack-contact-list/backend/models.py
@@ -1,10 +1,19 @@
 from config import db
 
+# creating the Contact class
+# - inherits from db.Model (a database model)
+# - can describe the different fields in the database as a Python object.
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     first_name = db.Column(db.String(80), unique = False, nullable = False)
     last_name = db.Column(db.String(80), unique = False, nullable = False)
     email = db.Column(db.String(120), unique = True, nullable = False)
 
+    # this function will convert our Contact object into a JSON object
     def to_json(self):
-        return {"id": self.id, "firstName": self.first_name, "lastName": self.last_name, "email": self.email}
+        return {
+                "id": self.id, 
+                "firstName": self.first_name, 
+                "lastName": self.last_name, 
+                "email": self.email
+        }
