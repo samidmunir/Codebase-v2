3617bfb4f4a4b962413081268f8359f04a955116
Sami Munir <sami.munir2001@gmail>
2024-09-10 21:40:32 -0400

    handling buffer sizes within client.py

--- Python Programming/Sockets/sockets-part-1/src/client.py
+++ Python Programming/Sockets/sockets-part-1/src/client.py
@@ -4,5 +4,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
 # this is a stream of data (so how big of chunks of data to receive at a time)
-message = s.recv(1024)
-print(message.decode('utf-8'))
+full_message = ''
+while True:
+    # message = s.recv(1024)
+    message = s.recv(8)
+    if len(message) <= 0:
+        break
+    full_message += message.decode('utf-8')
+print(full_message)
--- Python Programming/Sockets/sockets-part-1/src/server.py
+++ Python Programming/Sockets/sockets-part-1/src/server.py
@@ -8,3 +8,4 @@ while True:
     client_socket, address = s.accept()
     print(f'Connection from {address} has been established!')
     client_socket.send(bytes('Welcome to the server!', 'utf-8'))
+    client_socket.close()
