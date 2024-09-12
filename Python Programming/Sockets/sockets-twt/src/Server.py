48dc115ba64ecca6ae2e14bdc967bacbef2d8c01
Sami Munir <sami.munir2001@gmail>
2024-09-12 13:00:02 -0400

    implementing function handle_client() in Server.py: handling logic for multiple connections & safe disconnect protocol

--- Python Programming/Sockets/sockets-twt/src/Server.py
+++ Python Programming/Sockets/sockets-twt/src/Server.py
@@ -3,10 +3,16 @@ import threading
 import sys
 import time
 
+# Since we don't know the size of each message from the client,
+#   it always be 64.
+# - will have a number that indicates the number of bytes that we are to receive.
+HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 # print(f'SERVER: {SERVER}')
 ADDRESS = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
 
 # Setting up server socket.
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,7 +21,22 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(ADDRESS)
 
 def handle_client(connection, address):
-    pass
+    print(f'[NEW CONNECTION] - {address}')
+    connected = True
+    while connected:
+        # how many bytes to recieve from the client.
+        # - blocking line of code
+        # - will not proceed unless we recieve a message from the client.
+        message_length = connection.recv(HEADER).decode(FORMAT)
+        message_length = int(message_length)
+        message = connection.recv(message_length).decode(FORMAT)
+    
+        if message == DISCONNECT_MESSAGE:
+            connected = False
+        
+        print(f'\t[{address}] - {message}')
+        
+    connection.close()
 
 def start():
     # listening for new connections
@@ -31,6 +52,12 @@ def start():
         print(f'[ACTIVE CONNECTIONS] - {threading.activeAcount() - 1}')
 
 print('[STARTING] - server is spooling up')
+time.sleep(1)
+print('\t..')
+time.sleep(1)
+print('\t...')
 time.sleep(2)
+print('\t....')
+time.sleep(1)
 print('[READY] - server is ready and listening')
 start()
