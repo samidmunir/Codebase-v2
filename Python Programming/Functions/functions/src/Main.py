509420b5e1fa491bcb6579adb0664251ed79852a
Sami Munir <sami.munir2001@gmail>
2024-09-12 22:16:05 -0400

    implementing function get_time()

--- Python Programming/Functions/functions/src/Main.py
+++ Python Programming/Functions/functions/src/Main.py
@@ -3,4 +3,12 @@ def capitalize_name(name) -> str:
 
 name = 'sami munir'
 print(f'name: {name}')
-print(f'\ncalling capitalize_name({name}):\n--> {capitalize_name(name)}')
+print(f'\ncalling capitalize_name():\n--> {capitalize_name(name)}')
+
+from datetime import datetime
+
+def get_time() -> str:
+    now: datetime = datetime.now()
+    return f'{now:%X}'
+
+print(f'\ncalling get_time():\n--> {get_time()}')
