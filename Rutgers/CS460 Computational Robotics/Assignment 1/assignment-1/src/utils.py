7081c858c594b661eb39cd464762043882dda73c
Sami Munir <sami.munir2001@gmail>
2024-09-18 00:46:26 -0400

    implementing util functions random_quaternion() & quaternion_to_rotation_matrix()

--- Rutgers/CS460 Computational Robotics/Assignment 1/assignment-1/src/utils.py
+++ Rutgers/CS460 Computational Robotics/Assignment 1/assignment-1/src/utils.py
@@ -30,3 +30,28 @@ def euler_to_rotation_matrix(roll: float, pitch: float, yaw: float) -> np.ndarra
     
     # Combine the rotations: R = R_z * R_y * R_x
     return np.dot(R_z, np.dot(R_y, R_x))
+
+def random_quaternion() -> np.ndarray:
+    """
+        Generate a random unit quaternion.
+    """
+    u1, u2, u3 = np.random.uniform(0, 1, 3)
+    q1 = np.sqrt(1 - u1) * np.sin(2 * np.pi * u2)
+    q2 = np.sqrt(1 - u1) * np.cos(2 * np.pi * u2)
+    q3 = np.sqrt(u1) * np.sin(2 * np.pi * u3)
+    q4 = np.sqrt(u1) * np.cos(2 * np.pi * u3)
+    
+    return np.array([q1, q2, q3, q4])
+
+def quaternion_to_rotation_matrix(q: np.ndarray) -> np.ndarray:
+    """
+        Convert a quaternion (q1, q2, q3, q4) into a 3 x 3 rotation matrix.
+    """
+    q1, q2, q3, q4 = q
+    return np.array(
+        [
+            [1 - 2 * (q3 ** 2 + q4 ** 2), 2 * (q2 * q3 - q1 * q4), 2 * (q2 * q4 + q1 * q3)],
+            [2 * (q2 * q3 + q1 * q4), 1 - 2 * (q2 ** 2 + q4 ** 2), 2 * (q3 * q4 - q1 * q2)],
+            [2 * (q2 * q4 - q1 * q3), 2 * (q3 * q4 + q1 * q2), 1 - 2 * (q2 ** 2 + q3 ** 2)]
+        ]
+    )
