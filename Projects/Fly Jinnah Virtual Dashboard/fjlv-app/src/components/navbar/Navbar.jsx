c900e0d8f56fd0081bc4bd743ad8eada34584d6f
Sami Munir <sami.munir2001@gmail>
2024-09-30 19:34:04 -0400

    adding react-icons to navbar-list-items & correcting text/icon vertical alignment

--- Projects/Fly Jinnah Virtual Dashboard/fjlv-app/src/components/navbar/Navbar.jsx
+++ Projects/Fly Jinnah Virtual Dashboard/fjlv-app/src/components/navbar/Navbar.jsx
@@ -1,4 +1,7 @@
 import "./Navbar.css";
+import { LiaDesktopSolid, LiaHubspot, LiaBookmarkSolid, LiaBellSolid, LiaWhmcs } from "react-icons/lia";
+
+
 
 const Navbar = ({activeNavItem, setActiveNavItem}) => {
   return (
@@ -8,11 +11,11 @@ const Navbar = ({activeNavItem, setActiveNavItem}) => {
       </div>
       <div className="navbar-items-con news-cycle-regular">
         <ul className="navbar-items">
-            <li className={activeNavItem === 1 ? "active-item" : "navbar-item"} onClick={() => setActiveNavItem(1)}>Dashboard</li>
-            <li className={activeNavItem === 2 ? "active-item" : "navbar-item"} onClick={() => setActiveNavItem(2)}>Schedules</li>
-            <li className={activeNavItem === 3 ? "active-item" : "navbar-item"} onClick={() => setActiveNavItem(3)}>Bookings</li>
-            <li className={activeNavItem === 4 ? "active-item" : "navbar-item"} onClick={() => setActiveNavItem(4)}>NOTAMs</li>
-            <li className={activeNavItem === 5 ? "active-item" : "navbar-item"} onClick={() => setActiveNavItem(5)}>Settings</li>
+            <li className={activeNavItem === 1 ? "active-item" : "navbar-item"} onClick={() => setActiveNavItem(1)}>Dashboard <span className="list-item-icon"><LiaDesktopSolid /></span></li>
+            <li className={activeNavItem === 2 ? "active-item" : "navbar-item"} onClick={() => setActiveNavItem(2)}>Schedules <span className="list-item-icon"><LiaHubspot /></span></li>
+            <li className={activeNavItem === 3 ? "active-item" : "navbar-item"} onClick={() => setActiveNavItem(3)}>Bookings <span className="list-item-icon"><LiaBookmarkSolid /></span></li>
+            <li className={activeNavItem === 4 ? "active-item" : "navbar-item"} onClick={() => setActiveNavItem(4)}>NOTAMs <span className="list-item-icon"><LiaBellSolid /></span></li>
+            <li className={activeNavItem === 5 ? "active-item" : "navbar-item"} onClick={() => setActiveNavItem(5)}>Settings <span className="list-item-icon"><LiaWhmcs /></span></li>
         </ul>
       </div>
       <div className="navbar-controls-con news-cycle-regular">
