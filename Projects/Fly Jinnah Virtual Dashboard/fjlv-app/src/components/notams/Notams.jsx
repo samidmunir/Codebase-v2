1abc8029a98644fcc28d54e1279a06ae6b05ff47
Sami Munir <sami.munir2001@gmail>
2024-10-01 01:39:26 -0400

    working on NOTAMs component static + styling

--- Projects/Fly Jinnah Virtual Dashboard/fjlv-app/src/components/notams/Notams.jsx
+++ Projects/Fly Jinnah Virtual Dashboard/fjlv-app/src/components/notams/Notams.jsx
@@ -1,11 +1,53 @@
 import "./Notams.css";
 import Footer from "../footer/Footer";
+import notams_data from "../../data/notams/notams_data";
 
 const Notams = () => {
+    const notams = notams_data;
     return (
         <>
-            <div className="Notams">
-                <h1>Welcome to your NOTAMs...</h1>
+            <div className="Notams news-cycle-bold">
+                <div className="notams-title-con">
+                    <h1 className="notams-title">NOTAMs</h1>
+                </div>
+                <div className="notams-flex-con">
+                    {/* <div className="notams-info-con">
+                        <h2>Quick Overview</h2>
+                        <h3>{new Date().toLocaleTimeString()}</h3>
+                        <p>Total # of NOTAMS: 7</p>
+                        <p># of Acknowledged NOTAMs: 33</p>
+                        <p># of Unacknowledged NOTAMs: 5</p>
+                        <div className="recent-notam-con">
+                            <h4>Recent NOTAM</h4>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos nihil asperiores adipisci </p>
+                            <button className="news-cycle-bold">View</button>
+                        </div>
+                    </div> */}
+                    <div className="notams-carousel-con">
+                        {notams.map((notam) => {
+                            return (
+                                <div key={notam.notam_id}>
+                                    <p>{notam.notam_mssg}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className="create-notam-form-con">
+                    <h2>Create a new NOTAM</h2>
+                    <form>
+                        <label>New NOTAM</label>
+                        <input type="text" placeholder="Enter NOTAM..." maxLength={2500} />
+                        <label>Recieve by</label>
+                        <select>
+                            <option>To All</option>
+                            <option>To Staff</option>
+                            <option>To Pilots</option>
+                        </select>
+                        <label>Require Acknowledgement</label>
+                        <input type="checkbox" defaultChecked/>
+                    </form>
+                </div>
             </div>
             <Footer />
         </>
---
+++ Projects/Fly Jinnah Virtual Dashboard/fjlv-app/src/data/notams/notams_data.js
@@ -0,0 +1,29 @@
+const notams_data = [
+    {
+        "notam_id": 0,
+        "notam_time": new Date().toLocaleDateString(),
+        "notam_mssg": "lorem ipsumLorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+    {
+        "notam_id": 1,
+        "notam_time": new Date().toLocaleDateString(),
+        "notam_mssg": "lorem ipsumLorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+    {
+        "notam_id": 2,
+        "notam_time": new Date().toLocaleDateString(),
+        "notam_mssg": "lorem ipsumLorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+    {
+        "notam_id": 3,
+        "notam_time": new Date().toLocaleDateString(),
+        "notam_mssg": "lorem ipsumLorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+    {
+        "notam_id": 4,
+        "notam_time": new Date().toLocaleDateString(),
+        "notam_mssg": "lorem ipsumLorem ipsum dolor, sit amet consectetur adipisicing elit."
+    },
+]
+
+export default notams_data;
